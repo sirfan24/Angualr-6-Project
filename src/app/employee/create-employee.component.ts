3adcb45e5d6b2cf877d228b7171e89a53e1175b8
Syed Irfan <isyed@lsu.edu>
2020-05-19 17:51:56 -0500

    Part 5 - Angular Form Control and Form Group

---
+++ src/app/employee/create-employee.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms'
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent implements OnInit {
+
+  constructor() { }
+
+  employeeForm: FormGroup;
+ 
+  ngOnInit(): void {
+    this.employeeForm = new FormGroup({
+      fullName : new FormControl(),
+      email : new FormControl()
+    })
+  }
+
+  onSubmit(): void {
+
+    // Accessing FormGroup
+    console.log(this.employeeForm.value);
+
+    // Accessing FormControl
+    console.log(this.employeeForm.controls.fullName.touched);
+    console.log(this.employeeForm.get('fullName').value);
+
+  }
+
+}
