a88b4226c78f341d109a0a85be1cc8ed60d1a30e
Syed Irfan <isyed@lsu.edu>
2020-05-20 17:47:10 -0500

    Part - 9 Reactive Forms Validation

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms'
+import { FormGroup, FormBuilder, Validators} from '@angular/forms'
 
 @Component({
-  selector: 'app-create-employee',
+  selector: 'app-create-employee', 
   templateUrl: './create-employee.component.html',
   styleUrls: ['./create-employee.component.css']
 })
@@ -19,7 +19,8 @@ export class CreateEmployeeComponent implements OnInit {
 
     // Building reactive form using formBuilder class
     this.employeeForm = this.fb.group({
-      fullName:[''],
+      // All validator functions are sttaic functions, they dont need an instance.
+      fullName:['',[Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
       email:[''],
       skills: this.fb.group({
         skillName:[''],
